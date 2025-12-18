18ea1f0ae7ef682bc2129441bc442e472d099a99
Kseniia Yermilova <kseniia.yermilova@gmail.com>
2025-12-18 11:37:21 +0100

    Add miles to kilometers method

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,8 +1,16 @@
 package app;
 
 public class Main {
+    private static final double CONV_K = 1.60934d;
+
     static void main() {
         System.out.println("Convertor of length with SSH");
+        double mls = 3;
+        double kilometers = convMilesToKm(mls);
+        System.out.println("Result is " + kilometers + " km");
+    }
 
+    private static double convMilesToKm(double miles){
+        return miles * CONV_K;
     }
 }
